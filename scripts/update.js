7cd5b43793e39b068f8301d78e2e926d137c8064
ZepozeRasberry <p.evenslevasseur@yopmail.com>
2020-08-17 19:24:50 +0200

    way to resolve https://github.com/Zepoze/manladag-real-cli/issues/2

--- scripts/update.js
+++ scripts/update.js
@@ -4,6 +4,11 @@ nconf.file({dir: Path.join(__dirname,'..'),file: 'data.json'})
 
 module.exports = function(url) {
     try {
+        require.resolve(url)
+    } catch(e) {
+        return 0
+    }
+    try {
         const Source = require(url).Source
         nconf.set(`${Source.site.toLowerCase()}:mangas`, {})
         Object.keys(Source.mangas).forEach((e)=> {
@@ -11,8 +16,10 @@ module.exports = function(url) {
         })
         nconf.set(`${Source.site.toLowerCase()}:module`,url)
         nconf.save()
-        
+
+        console.log(`the source ${url} will be added`)
+        return 1
     } catch(e) {
-        
+        throw new Error(`the source ${url} will be not added because something get wrong\n${e.message}`)
     }
 }
